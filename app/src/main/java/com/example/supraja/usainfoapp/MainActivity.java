d74efae8a31bbd0c97ababe7dbe746d0d9919177
Supraja1001 <supraja.ayyamgari@gmail.com>
2017-06-08 15:44:54 -0400

    Initial commit - Supraja

---
+++ app/src/main/java/com/example/supraja/usainfoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.supraja.usainfoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
